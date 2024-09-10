dd3058d56b2753fd8c167ab1f31ee2bb8acd7baf
Steven Cai <szcai@ad.unc.edu>
2024-09-10 14:53:39 -0400

    add color select button

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
 "use strict";
 
-const img = document.getElementById("charizard");
-console.log("hi");
+const img = document.querySelector(".pokemon-img");
 const changeImage = function () {
-  console.log(img.id);
-  console.log(img.src === null);
-  if (img.id === "charizard") {
-    if (img.src.endswith("./images/006.png"))
+  if (img.id === "charizard-img") {
+    if (img.src.includes("006.png")) {
       img.src = "./images/shiny-charizard.png";
-    else img.src = "./images/006.png";
+    } else {
+      img.src = "./images/006.png";
+    }
+  } else if (img.id === "blastoise-img") {
+    if (img.src.includes("009.png")) {
+      img.src = "./images/shiny-blastoise.png";
+    } else {
+      img.src = "./images/009.png";
+    }
+  } else {
+    if (img.src.includes("003.png")) {
+      img.src = "./images/shiny-venusaur.png";
+    } else {
+      img.src = "./images/003.png";
+    }
   }
 };
